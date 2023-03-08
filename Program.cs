7f334a63236870421984f1dcbe18d92e1398d42b
Pip Matthews <pip.matthews+github@gmail.com>
2023-03-07 20:52:46 -0500

    Here I describe my changes

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            int numberOfCupsOfCoffee = 5;
+            string fullName = "Philippa Matthews";
+            var today = "03/07/2023";
+            Console.WriteLine($"My 3 Variables are {numberOfCupsOfCoffee}, {fullName} and {today}.");
+
+        }
     }
-  }
 }
